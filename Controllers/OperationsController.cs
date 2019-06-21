c8442afe5ea1375921ba0a5a7e898d6a383f3285
glautrou <g.lautrou@outlook.com>
2019-06-21 23:45:34 +0200

    Add empty definitions

---
+++ Controllers/OperationsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RapsoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public void Import([FromBody] string data)
+        {
+        }
+
+        // [HttpGet("{id}")]
+        // public ActionResult<string> Get(int id)
+        // {
+        //     return "value";
+        // }
+
+        // [HttpPost]
+        // public void Post([FromBody] string value)
+        // {
+        // }
+
+        // [HttpPut("{id}")]
+        // public void Put(int id, [FromBody] string value)
+        // {
+        // }
+
+        // [HttpDelete("{id}")]
+        // public void Delete(int id)
+        // {
+        // }
+    }
+}
