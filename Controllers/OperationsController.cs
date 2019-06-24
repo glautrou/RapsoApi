bde6fc45c46e14f939c81d8a5742bb72cea688fc
glautrou <g.lautrou@outlook.com>
2019-06-24 23:13:19 +0200

    Start import implementation

--- Controllers/OperationsController.cs
+++ Controllers/OperationsController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RapsoApi.Model;
+using CsvHelper;
+using System.Net.Http;
+using System.IO.Compression;
 
 namespace RapsoApi.Controllers
 {
@@ -17,8 +22,37 @@ namespace RapsoApi.Controllers
         }
 
         [HttpPost]
-        public void Import([FromBody] string data)
+        public async Task Import(int year)
         {
+            //check if already imported
+
+            //download file
+            var url = $"https://cadastre.data.gouv.fr/data/etalab-dvf/latest/csv/{year}/full.csv.gz";
+            var stream = await new HttpClient().GetStreamAsync(url);
+            var file = new MemoryStream();
+            using(var decompressionStream = new GZipStream(stream, CompressionMode.Decompress))
+            {
+                decompressionStream.CopyTo(file);
+
+            }
+
+            using (var reader = new StreamReader(file))
+            using (var csv = new CsvReader(reader))
+            {
+                var operations = csv.GetRecords<Operation>();
+
+            }
+        }
+
+        public static Stream Decompress(Stream stream)
+        {
+            var result = new MemoryStream();
+            using (GZipStream decompressionStream = new GZipStream(stream, CompressionMode.Decompress))
+            {
+                decompressionStream.CopyTo(result);
+            }
+
+            return result;
         }
 
         // [HttpGet("{id}")]
---
+++ Model/Operation.cs
@@ -0,0 +1,45 @@
+namespace RapsoApi.Model
+{
+    public class Operation
+    {
+        public string id_mutation { get; set; }
+        public string date_mutation { get; set; }
+        public string numero_disposition { get; set; }
+        public string valeur_fonciere { get; set; }
+        public string adresse_numero { get; set; }
+        public string adresse_suffixe { get; set; }
+        public string adresse_code_voie { get; set; }
+        public string adresse_nom_voie { get; set; }
+        public string code_postal { get; set; }
+        public string code_commune { get; set; }
+        public string nom_commune { get; set; }
+        public string ancien_code_commune { get; set; }
+        public string ancien_nom_commune { get; set; }
+        public string code_departement { get; set; }
+        public string id_parcelle { get; set; }
+        public string ancien_id_parcelle { get; set; }
+        public string numero_volume { get; set; }
+        public string lot_1_numero { get; set; }
+        public string lot_1_surface_carrez { get; set; }
+        public string lot_2_numero { get; set; }
+        public string lot_2_surface_carrez { get; set; }
+        public string lot_3_numero { get; set; }
+        public string lot_3_surface_carrez { get; set; }
+        public string lot_4_numero { get; set; }
+        public string lot_4_surface_carrez { get; set; }
+        public string lot_5_numero { get; set; }
+        public string lot_5_surface_carrez { get; set; }
+        public string nombre_lots { get; set; }
+        public string code_type_local { get; set; }
+        public string type_local { get; set; }
+        public string surface_reelle_bati { get; set; }
+        public string nombre_pieces_principales { get; set; }
+        public string code_nature_culture { get; set; }
+        public string nature_culture { get; set; }
+        public string code_nature_culture_speciale { get; set; }
+        public string nature_culture_speciale { get; set; }
+        public string surface_terrain { get; set; }
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+    }
+}
